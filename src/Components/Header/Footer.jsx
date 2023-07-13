4b972772ddbbea69904e8ac48334b4fec038a8e6
Praveen <praveenchandraravuri@gmail.com>
2023-07-13 11:01:31 +0530

    ecommainfrontend

---
+++ src/Components/Header/Footer.jsx
@@ -0,0 +1,42 @@
+
+import React from "react";
+
+const Footer = () => {
+  return (
+ <footer>
+      <div className="shippringContainer">
+        <div className="shippingContainerItems">
+          <h3>Ishop</h3>
+          <p>
+            E-commerce is revolutionizing the way we all shop in India. Why do
+            you want to hop from one store to another in search of the latest
+            phone when you can find it on the Internet in a single click? Not
+            only mobiles. Ishop houses everything you can possibly imagine, from
+            trending electronics like laptops, tablets, smartphones, and mobile
+            accessories to in-vogue fashion staples like shoes, clothing and
+            lifestyle accessories.
+          </p>
+        </div>
+        <div className="shippingContainerItems">
+          <h3>Contact Us</h3>
+          <p>
+            Ishop Private Limited,Building Name , Address Line 1, Address Line
+            2, Nearest Location, City, PinCode, State, India
+          </p>
+        </div>
+        <div className="shippingContainerItems">
+          <h3>Follow Us</h3>
+          <p>
+            You can follow us on social media as we are everywhere for our
+            costomers. <br />
+            <br />
+            Instagram <br />
+            Facebook{" "}
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Header/RouteLinks.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import SignUp from "../Auth/SignUp";
+import Details from "../Compo/Details";
+import Login from "../Auth/Login";
+import Fashion from '../pages/Fashion';
+import Jewelery from '../pages/Jewelery';
+import Electronic from '../pages/Electronic';
+import Profile from '../UserProfile/Profile';
+import Order from '../UserProfile/Order';
+import ShowCart from '../Cart/ShowCart';
+import PersonalInfo from '../UserProfile/PersonalInfo';
+import Store from '../pages/Store';
+
+const RouteLinks = () => {
+    return (
+        <div>
+            <Routes>
+            <Route path="/" element={<Home /> } />
+              <Route path="/signup" element={<SignUp />} />
+                <Route path="/products/:category/:id" element={<Details />} />
+                  <Route path="/SignIn" element={<Login />} />
+                   <Route path='/fashion' element={<Fashion/>}/>
+            <Route path='/jewelery' element={<Jewelery/>}/>
+            <Route path='/electronics' element={<Electronic/>}/>
+            <Route path='/user' element={<Profile/>}>
+              <Route path='profile' element={<PersonalInfo/>}/>
+              <Route path='orders' element={<Order/>}/>
+              <Route path='giftcard' element={<Order/>}/>
+            </Route>
+            <Route path='/cart' element={<ShowCart/>}/>
+            <Route path='/store' element={<Store/>}></Route>
+
+                       </Routes>    
+
+        </div>
+    )
+}
+export default RouteLinks;
